e98e09a5ef040358c4380613b4024106858ec595
Sasi Patterson <sweetpea_th@hotmail.com>
2022-08-23 15:46:12 -0400

    created new function to get the welcome page and quiz-box display/hidden accordingly

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,17 @@
+
 var startButton = document.querySelector('.btn-start');
 var timeEl = document.querySelector('.tik-tok');
+var quizBox = document.querySelector('.quiz-box');
 var quizz1 = document.querySelector('#quiz1');
 var quizz2 = document.querySelector('#quiz2');
 var quizz3 = document.querySelector('#quiz3');
-var buttons = document.querySelectorAll('choice');
+var buttons = document.querySelectorAll('.choice');
+var intro = document.querySelector('.intro');
+var position = 1;
 
 // To countdown the time 
 var timeLeft = function() {
-    var timeRemain = 10;
+    var timeRemain = 75;
 
     var timeInterval = setInterval(function() {
         timeRemain--;
@@ -22,33 +26,61 @@ var timeLeft = function() {
 };
 
 // Once the start button is clicked the time will start counting down
-startButton.addEventListener('click', timeLeft);
- 
+startButton.addEventListener('click', function(){
+    timeLeft();
+    setState("quizBox");
+});
+
+
 // var questionOne = [
 //     {   title: "Question1",
 //         choices : ['Andrew', 'Sasima', 'Keith', 'Tara'],
 //         answer: 'Sasima'
 //     }
 // ]
+var setState = function (state){
 
+    intro.style.display = 'none';
+    quizBox.style.display = 'none';
 
-// buttons.forEach(function(button){
-//     button.addEventListener('click', function(){
-//         var answer = button.textContent;
+    if(state === "intro"){
+        intro.style.display = 'block';
+    }
+    if(state === "quizBox"){
+        quizBox.style.display = 'block';
+    }
+}
 
-//         if(answer === this.choices[1]) {
-//             quizz1.textContent = 'Correct';
-//             quizz1.style.display = 'none';
-//             quizz2.style.display = 'block';
-//         } else {
-//             quizz1.textContent = 'Wrong';
-//             quizz1.style.display = 'none';
-//             quizz2.style.display = 'block';
-//         }
-        
-//     })
-// })
 // console.log(answer);
 // quizz1.style.display = 'none';
 quizz2.style.display = 'none';
 quizz3.style.display = 'none';
+var displayNextQuestion = function (parentNode){
+    position++;
+    parentNode.style.display = 'none';
+    var nextQuestionEl = document.querySelector('#quiz' + position);
+    nextQuestionEl.style.display = null;
+}
+document.addEventListener('click', function(event){
+    // var answer = buttons.textContent;
+    if(event.target.matches('.choice')){
+        var element = event.target;
+        var parent = element.parentNode;
+        console.log(element.textContent);
+        if(element.textContent === parent.dataset.answer){
+            console.log('correct');
+        }
+        displayNextQuestion(parent);
+    }
+    // if(answer === this.choices[1]) {
+    //     quizz1.textContent = 'Correct';
+    //     quizz1.style.display = 'none';
+    //     quizz2.style.display = 'block';
+    // } else {
+    //     quizz1.textContent = 'Wrong';
+    //     quizz1.style.display = 'none';
+    //     quizz2.style.display = 'block';
+    // }
+    // 
+});
+setState("intro");
