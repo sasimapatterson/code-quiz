3bdb9eaafb086763172fea3c7e63144e91bfbc90
Sasi Patterson <sweetpea_th@hotmail.com>
2022-08-22 20:06:48 -0400

    still trying to figure out the questions section.

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,8 @@
 var startButton = document.querySelector('.btn-start');
 var timeEl = document.querySelector('.tik-tok');
 var quizz1 = document.querySelector('#quiz1');
+var quizz2 = document.querySelector('#quiz2');
+var quizz3 = document.querySelector('#quiz3');
 
 // To countdown the time 
 var timeLeft = function() {
@@ -21,8 +23,17 @@ var timeLeft = function() {
 // Once the start button is clicked the time will start counting down
 startButton.addEventListener('click', timeLeft);
  
+var questionOne = ['Andrew', 'Sasima', 'Keith', 'Tara'];
+
+
+if (answer === 'Sasima'){
+    console.log("Congrats");
+} else {
+    console.log("try again");
+}
 
 
 
-
-// quizz1.style.visibility = 'hidden';
+// quizz1.style.display = 'none';
+quizz2.style.display = 'none';
+quizz3.style.display = 'none';
