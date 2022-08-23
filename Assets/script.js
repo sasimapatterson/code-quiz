09f310e41485615f3fc88786bd5c7e19fec3afdc
Sasi Patterson <sweetpea_th@hotmail.com>
2022-08-23 17:29:35 -0400

    added endQuiz function to move to the score page after all quizzes were answered.

--- Assets/script.js
+++ Assets/script.js
@@ -5,28 +5,33 @@ var quizBox = document.querySelector('.quiz-box');
 var quizz1 = document.querySelector('#quiz1');
 var quizz2 = document.querySelector('#quiz2');
 var quizz3 = document.querySelector('#quiz3');
+var quizz4 = document.querySelector('#quiz4');
 var buttons = document.querySelectorAll('.choice');
 var intro = document.querySelector('.intro');
+var scorePage = document.querySelector('#score')
 var position = 1;
+var timeInterval;
+
+var score = 0;
 
 // To countdown the time 
-var timeLeft = function() {
+var timeLeft = function () {
     var timeRemain = 75;
 
-    var timeInterval = setInterval(function() {
+    timeInterval = setInterval(function () {
         timeRemain--;
         timeEl.textContent = timeRemain;
 
-        if(timeRemain === 0) {
+        if (timeRemain === 0) {
             clearInterval(timeInterval);
             console.log()
         }
     }, 1000);
-    
+
 };
 
 // Once the start button is clicked the time will start counting down
-startButton.addEventListener('click', function(){
+startButton.addEventListener('click', function () {
     timeLeft();
     setState("quizBox");
 });
@@ -38,16 +43,20 @@ startButton.addEventListener('click', function(){
 //         answer: 'Sasima'
 //     }
 // ]
-var setState = function (state){
+var setState = function (state) {
 
     intro.style.display = 'none';
     quizBox.style.display = 'none';
+    scorePage.style.display = 'none';
 
-    if(state === "intro"){
+    if (state === "intro") {
         intro.style.display = 'block';
     }
-    if(state === "quizBox"){
+    if (state === "quizBox") {
         quizBox.style.display = 'block';
+    }
+    if (state === "score") {
+        scorePage.style.display = 'block';
     }
 }
 
@@ -55,32 +64,34 @@ var setState = function (state){
 // quizz1.style.display = 'none';
 quizz2.style.display = 'none';
 quizz3.style.display = 'none';
-var displayNextQuestion = function (parentNode){
+quizz4.style.display = 'none';
+var displayNextQuestion = function (parentNode) {
     position++;
     parentNode.style.display = 'none';
     var nextQuestionEl = document.querySelector('#quiz' + position);
     nextQuestionEl.style.display = null;
 }
-document.addEventListener('click', function(event){
+document.addEventListener('click', function (event) {
     // var answer = buttons.textContent;
-    if(event.target.matches('.choice')){
+    if (event.target.matches('.choice')) {
         var element = event.target;
         var parent = element.parentNode;
         console.log(element.textContent);
-        if(element.textContent === parent.dataset.answer){
-            console.log('correct');
+        if (element.textContent === parent.dataset.answer) {
+            score++;
         }
-        displayNextQuestion(parent);
+        if (parent.dataset.answer === "c.quotes") {
+            endQuiz();
+        } else {
+            displayNextQuestion(parent);
+        }
+
     }
-    // if(answer === this.choices[1]) {
-    //     quizz1.textContent = 'Correct';
-    //     quizz1.style.display = 'none';
-    //     quizz2.style.display = 'block';
-    // } else {
-    //     quizz1.textContent = 'Wrong';
-    //     quizz1.style.display = 'none';
-    //     quizz2.style.display = 'block';
-    // }
-    // 
 });
+
+var endQuiz = function(){
+    clearInterval(timeInterval);
+    setState("score");
+}
+
 setState("intro");
